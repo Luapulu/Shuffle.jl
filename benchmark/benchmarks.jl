35f5887040777763401d9ab01cb0148b821e03f1
Paul Nemec <paul.nemec@tum.de>
2020-11-08 14:36:45 +0100

    Add benchmarks

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,48 @@
+using BenchmarkTools, Shuffle
+
+const SUITE = BenchmarkGroup()
+
+const DECK = collect(1:52)
+
+## Faro ##
+
+SUITE["Faro"] = BenchmarkGroup(["deterministic"])
+SUITE["Faro"]["in"] = BenchmarkGroup()
+SUITE["Faro"]["out"] = BenchmarkGroup()
+
+SUITE["Faro"]["in"]["shuffle"] = @benchmarkable shuffle($DECK, Faro{:in}())
+
+SUITE["Faro"]["in"]["shuffle!"] = @benchmarkable shuffle!($DECK, Faro{:in}())
+
+SUITE["Faro"]["in"]["nshuffle"] = @benchmarkable nshuffle($DECK, 26, Faro{:in}())
+
+SUITE["Faro"]["in"]["nshuffle!"] = @benchmarkable nshuffle!($DECK, 26, Faro{:in}())
+
+SUITE["Faro"]["out"]["shuffle"] = @benchmarkable shuffle($DECK, Faro{:out}())
+
+SUITE["Faro"]["out"]["shuffle!"] = @benchmarkable shuffle!($DECK, Faro{:out}())
+
+SUITE["Faro"]["out"]["nshuffle"] = @benchmarkable nshuffle($DECK, 26, Faro{:out}())
+
+SUITE["Faro"]["out"]["nshuffle!"] = @benchmarkable nshuffle!($DECK, 26, Faro{:out}())
+
+## RandomShuffle ##
+
+SUITE["Random"] = BenchmarkGroup(["random"])
+
+SUITE["Random"]["shuffle"] = @benchmarkable shuffle($DECK, RandomShuffle())
+
+SUITE["Random"]["shuffle!"] = @benchmarkable shuffle!($DECK, RandomShuffle())
+
+## GilbertShannonReeds ##
+
+SUITE["GilbertShannonReeds"] = BenchmarkGroup(["random"])
+
+SUITE["GilbertShannonReeds"]["shuffle"] =
+    @benchmarkable shuffle($DECK, GilbertShannonReeds())
+
+SUITE["GilbertShannonReeds"]["nshuffle"] =
+    @benchmarkable nshuffle($DECK, 7, GilbertShannonReeds())
+
+SUITE["GilbertShannonReeds"]["nshuffle!"] =
+    @benchmarkable nshuffle!($DECK, 7, GilbertShannonReeds())
